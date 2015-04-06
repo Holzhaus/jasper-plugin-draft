7dc5b07ac3cea71ccc267042c253dd68993e184d
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2015-04-06 18:05:52 +0200

    Support disabling plugins via configuration

--- core/pluginmanager.py
+++ core/pluginmanager.py
@@ -243,11 +243,29 @@ class PluginConfigDecorator(
         plugin_object.register_option = plugin_register_option
         plugin_object.get_option = plugin_get_option
 
+    def __decorate_activate_method(self, plugin_name, plugin_object):
+        plugin_object.register_option('enabled',
+                                      default_value=True,
+                                      is_boolean=True)
+
+        ORIGINAL_METHOD = plugin_object.activate
+        def plugin_activate():
+            if plugin_object.get_option('enabled'):
+                ORIGINAL_METHOD()
+                self._logger.info("Plugin '%s' activated.",
+                                  plugin_name)
+            else:
+                self._logger.info("Plugin '%s' disabled in configuration.",
+                                  plugin_name)
+        plugin_object.activate = plugin_activate
+
     def loadPlugins(self, *args, **kwargs):
         self._component.loadPlugins(*args, **kwargs)
         for plugin_info in self._component.getAllPlugins():
             self.__add_config_methods(plugin_info.slug,
                                       plugin_info.plugin_object)
+            self.__decorate_activate_method(plugin_info.name,
+                                            plugin_info.plugin_object)
             plugin_info.plugin_object.configure()
 
 
